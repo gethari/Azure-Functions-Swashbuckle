4ef1c80355716093282e1e25601f2b9b4e0bdcac
HariHaran <hharan618@gmail.com>
2020-05-10 17:55:57 +0530

    Azure function swashbuckle

---
+++ FunctionApp1/FunctionApp1/Startup.cs
@@ -0,0 +1,17 @@
+﻿using AzureFunctions.Extensions.Swashbuckle;
+using AzureFunctions_SwashBuckle;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using System.Reflection;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace AzureFunctions_SwashBuckle
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ FunctionApp1/FunctionApp1/SwaggerJson.cs
@@ -0,0 +1,24 @@
+﻿using AzureFunctions.Extensions.Swashbuckle;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AzureFunctions_SwashBuckle
+{
+    public class SwaggerJson
+    {
+        [FunctionName("SwaggerJson")]
+        [SwaggerIgnore]
+        public static Task<HttpResponseMessage> Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "json")]
+        HttpRequestMessage req,
+    ILogger log,
+    [SwashBuckleClient] ISwashBuckleClient swashBuckleClient)
+        {
+            return Task.FromResult(swashBuckleClient.CreateSwaggerDocumentResponse(req));
+        }
+    }
+}
